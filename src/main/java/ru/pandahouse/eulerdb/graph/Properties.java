0622a90420ce8f210f91e8048e3dd6cd39115d53
AlexUnderIT <alexunderit@yandex.ru>
2024-01-15 14:47:13 +0300

    добавил пример в коммент, чтоб был перед глазами

--- src/main/java/ru/pandahouse/eulerdb/graph/Properties.java
+++ src/main/java/ru/pandahouse/eulerdb/graph/Properties.java
@@ -1,7 +1,17 @@
 package ru.pandahouse.eulerdb.graph;
 
 public class Properties {
-
+    /*
+    * {
+    * "id": "1c26e2995e72287951ef75ce76f93991ed157e69",
+    * "shortId": "1c26e2995",
+    * "refs": "",
+    * "authorId": "max",
+    * authorEmail: "dbi471@gmail.com",
+    * title: "конфиг папки добавил, а то потерялись",
+    * parents: "41276e25e2803a6b2d7a634100e590eb2f2f5307"
+    * }
+    */
     private Long propId;
     private Long nodeId;
     private String author;

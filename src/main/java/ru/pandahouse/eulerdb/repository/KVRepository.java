c3de657a918ae630fe3f2467a53a8113f8af90c7
AlexUnderIT <alexunderit@yandex.ru>
2024-01-16 13:35:51 +0300

    Добавил репозиторий и поменял логику сервиса

---
+++ src/main/java/ru/pandahouse/eulerdb/repository/KVRepository.java
@@ -0,0 +1,11 @@
+package ru.pandahouse.eulerdb.repository;
+
+import java.util.Optional;
+
+public interface KVRepository<K, V> {
+    boolean save(K key, V value);
+
+    Optional<V> find(K key);
+
+    boolean delete(K key);
+}

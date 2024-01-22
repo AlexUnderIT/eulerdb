1b8d602d08ffde9b8b793ebb2a8a29fb3e493734
AlexUnderIT <alexunderit@yandex.ru>
2024-01-22 16:51:53 +0300

    Добавил чуть логику: теперь для каждого коммита можно добавить posting list  дочерних коммитов

--- src/main/java/ru/pandahouse/eulerdb/controller/IndexController.java
+++ src/main/java/ru/pandahouse/eulerdb/controller/IndexController.java
@@ -1,8 +1,12 @@
 package ru.pandahouse.eulerdb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.pandahouse.eulerdb.service.RocksDbService;
+
+import java.sql.SQLOutput;
+import java.util.Optional;
 
 @RestController
 public class IndexController {
@@ -19,5 +23,17 @@ public class IndexController {
     public void get() {
         rocksDbService.get();
     }
-
+    @GetMapping("/put/{key}/{value}")
+    public void putSomething(@PathVariable("key") String key,
+                             @PathVariable("value") Object value){
+        rocksDbService.save(key, value);
+    }
+    @GetMapping("/find/{key}")
+    public void getSmthByKey(@PathVariable("key") String key){
+        rocksDbService.find(key);
+    }
+    @GetMapping("/delete/{key}")
+    public void deleteSmthByKey(@PathVariable("key") String key){
+        rocksDbService.delete(key);
+    }
 }
--- src/main/java/ru/pandahouse/eulerdb/repository/KVRepository.java
+++ src/main/java/ru/pandahouse/eulerdb/repository/KVRepository.java
@@ -1,11 +1,12 @@
 package ru.pandahouse.eulerdb.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface KVRepository<K, V> {
     boolean save(K key, V value);
 
-    Optional<V> find(K key);
+    Optional<List<V>> find(K key);
 
     boolean delete(K key);
 }
